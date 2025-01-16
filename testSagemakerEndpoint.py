38bd456900951ffcba22f461df017d50b195f690
Ben Fields <techpotato22@gmail.com>
2025-01-16 10:48:45 -0800

    updated test script.

--- testSagemakerEndpoint.py
+++ testSagemakerEndpoint.py
@@ -1,38 +1,39 @@
 import numpy as np
 import sagemaker
-from sagemaker.deserializers import JSONDeserializer
 from sagemaker.predictor import Predictor
 from sagemaker.serializers import JSONSerializer
+from sagemaker.deserializers import JSONDeserializer
 
 session = sagemaker.Session()
-endpoint_name = "tensorflow-inference-endpoint-name"
+endpoint_name = 'sagemaker-endpoint-name'
 
 # Create a generic Predictor
 predictor = Predictor(
     endpoint_name=endpoint_name,
     sagemaker_session=session,
     serializer=JSONSerializer(),
-    deserializer=JSONDeserializer(),
+    deserializer=JSONDeserializer()
 )
-counter = 0
-while 1:
-    # sleep for 1 second
+while(1):
+    #sleep for 1 second
     import time
-
     time.sleep(1)
-    if counter == 0:
-        sample_input = np.array([1, 0, 0, 0])
-    elif counter == 1:
-        sample_input = np.array([0, 1, 0, 0])
-    elif counter == 2:
-        sample_input = np.array([0, 0, 1, 0])
-    elif counter == 3:
-        sample_input = np.array([0, 0, 0, 1])
+    sample_input = np.random.randint(1,1000,4)
     predictions = predictor.predict(sample_input)
+    print("Input: ",sample_input)
     print("Raw predictions:", predictions)
-    prediction_vector = predictions["predictions"][0]
-    print("Predicted class:", np.argmax(prediction_vector))
-    print("Success")
-    counter += 1
-    if counter > 3:
-        counter = 0
+    prediction_vector = predictions['predictions'][0]
+    infer = np.argmax(prediction_vector)
+    check = np.argmax(sample_input)
+    print("Prediction: ", infer )
+    print("Max of input: ", check)
+    if infer == check:
+        print("Correct")
+    else:
+        print("Incorrect")
+    
+
+
+
+
+
